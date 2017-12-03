1333bd26837ef7e42a3e79e5e3cb253821dcb646
Spotlight <awesomebing1@gmail.com>
2017-12-03 00:24:17 -0600

    Don't crash over no error
    Let's not crash over a lack of spilt milk, lol

--- delete.go
+++ delete.go
@@ -2,23 +2,30 @@ package main
 
 import (
 	"database/sql"
-	"net/http"
-	"log"
 	"fmt"
+	"log"
+	"net/http"
 )
 
 func Delete(w http.ResponseWriter, r *http.Request, db *sql.DB) {
 	stmt, err := db.Prepare("DELETE FROM `mails` WHERE `sent` = 1 AND `recipient_id` = ? ORDER BY `timestamp` ASC LIMIT ?")
 	if err != nil {
-		log.Fatal(stmt)
+		panic(err)
+		// Welp, that went downhill fast.
+		w.Write([]byte(genNormalErrorCode(450, "Database error.")))
+		return
 	}
 	r.ParseForm()
 
-	wiiID := r.Form.Get("mlid")[1:]
+	wiiID := r.Form.Get("mlid")
 	delnum := r.Form.Get("delnum")
-	log.Fatal(stmt.Exec(wiiID, delnum))
+	_, err = stmt.Exec(wiiID, delnum)
 
-	if stmt != nil {
+	if err != nil {
+		log.Fatal(err)
+		w.Write([]byte(fmt.Sprint("cd=541\n",
+			"msg=Issue deleting mail from database.\n")))
+	} else {
 		w.Write([]byte(fmt.Sprint("cd=100\n",
 			"msg=Success.\n",
 			"delnum=", delnum)))
