fa49006118000cdb85db88e36f4e86f96730513a
Spotlight <spotlight@joscomputing.space>
2018-02-10 16:18:24 -0600

    Add docker support

---
+++ schema/1-create-mail-tables.sql
@@ -0,0 +1,81 @@
+-- MySQL dump 10.13  Distrib 5.7.18, for Linux (x86_64)
+--
+-- Host: localhost    Database: WC24Mail
+-- ------------------------------------------------------
+-- Server version	5.7.18-0ubuntu0.16.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `accounts`
+--
+
+CREATE DATABASE IF NOT EXISTS WC24Mail;
+USE WC24Mail;
+DROP TABLE IF EXISTS `accounts`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `accounts` (
+  `mlid` varchar(17) NOT NULL,
+  `passwd` varchar(16) DEFAULT NULL,
+  `mlchkid` varchar(32) DEFAULT NULL,
+  `ip` int(11) DEFAULT NULL COMMENT 'Long2IP - used for associating check script with send/receive/etc',
+  UNIQUE KEY `mlid` (`mlid`),
+  UNIQUE KEY `mlchkid` (`mlchkid`),
+  UNIQUE KEY `passwd` (`passwd`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='WiiConnect24 "Accounts"';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `mails`
+--
+
+DROP TABLE IF EXISTS `mails`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `mails` (
+  `mail_id` varchar(255) NOT NULL,
+  `message_id` varchar(255) NOT NULL,
+  `sender_wiiID` varchar(17) DEFAULT NULL,
+  `mail` mediumtext,
+  `recipient_id` varchar(16) DEFAULT NULL,
+  `sent` tinyint(1) NOT NULL DEFAULT '0',
+  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`mail_id`),
+  UNIQUE KEY `mail_id` (`mail_id`),
+  KEY `message_id` (`message_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='Sent mails from send.cgi';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `stats`
+--
+
+DROP TABLE IF EXISTS `stats`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `stats` (
+  `WiiFC` varchar(16) NOT NULL,
+  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='Statistical data';
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2017-07-30 10:47:30
