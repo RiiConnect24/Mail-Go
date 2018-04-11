f372330e3ed5dbdd2fc0ed9188831f977a9affb5
Larsenv <larsen@weborican.com>
2018-04-11 18:50:30 -0500

    Remove statuscreated header

--- receive.go
+++ receive.go
@@ -66,7 +66,6 @@ func Receive(w http.ResponseWriter, r *http.Request, db *sql.DB) {
 	// Loop through mail and make the output.
 	wc24MimeBoundary := fmt.Sprint("BoundaryForDL", time.Now().Format("200601021504"), "/", random(1000000, 9999999))
 	w.Header().Add("Content-Type", fmt.Sprint("multipart/mixed; boundary=", wc24MimeBoundary))
-	w.WriteHeader(http.StatusCreated)
 
 	defer storedMail.Close()
 	for storedMail.Next() {
