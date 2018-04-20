da5bfe4bd75d01239804276ef8b87893fbfa8467
Spotlight <spotlight@joscomputing.space>
2018-04-19 19:07:38 -0500

    Add header + duplicate registration checking

--- account.go
+++ account.go
@@ -12,14 +12,17 @@ import (
 func Account(w http.ResponseWriter, r *http.Request) {
 	wiiID := r.Form.Get("mlid")
 	if !friendCodeIsValid(wiiID) {
+		w.Header().Add("cd", "610")
 		fmt.Fprint(w, GenNormalErrorCode(610, "Invalid Wii Friend Code."))
+		return
 	}
 
 	w.Header().Add("Content-Type", "text/plain;charset=utf-8")
 
 	stmt, err := db.Prepare("INSERT IGNORE INTO `accounts` (`mlid`,`passwd`, `mlchkid` ) VALUES (?, ?, ?)")
 	if err != nil {
-		fmt.Fprint(w, GenNormalErrorCode(450, "Database error."))
+		w.Header().Add("cd", "410")
+		fmt.Fprint(w, GenNormalErrorCode(410, "Database error."))
 		log.Fatal(err)
 		return
 	}
@@ -32,13 +35,32 @@ func Account(w http.ResponseWriter, r *http.Request) {
 	mlchkidByte := sha512.Sum512(append(salt, []byte(mlchkid)...))
 	mlchkidHash := hex.EncodeToString(mlchkidByte[:])
 
-	_, err = stmt.Exec(wiiID, passwdHash, mlchkidHash)
+	result, err := stmt.Exec(wiiID, passwdHash, mlchkidHash)
 	if err != nil {
-		fmt.Fprint(w, GenNormalErrorCode(450, "Database error."))
+		w.Header().Add("cd", "410")
+		fmt.Fprint(w, GenNormalErrorCode(410, "Database error."))
 		log.Println(err)
 		return
 	}
 
+	affected, err := result.RowsAffected()
+	if err != nil {
+		w.Header().Add("cd", "410")
+		fmt.Fprint(w, GenNormalErrorCode(410, "Database error."))
+		log.Println(err)
+		return
+	}
+
+	if affected == 0 {
+		w.Header().Add("cd", "211")
+		fmt.Fprint(w, "\n",
+			GenNormalErrorCode(211, "Duplicate registration."))
+		return
+	}
+
+	w.Header().Add("cd", "100")
+	w.Header().Add("mlchkid", mlchkid)
+	w.Header().Add("passwd", passwd)
 	fmt.Fprint(w, "\n",
 		GenNormalErrorCode(100, "Success."),
 		"mlid=", wiiID, "\n",
