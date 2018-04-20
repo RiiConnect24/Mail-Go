e0f290139120969aa12943374832a446c2c9034b
Spotlight <spotlight@joscomputing.space>
2018-04-19 21:12:25 -0500

    Remove headers
    That wasn't necessary; I just can't read and failed to notice the Homebrew was faking a header-like format.

--- account.go
+++ account.go
@@ -12,7 +12,6 @@ import (
 func Account(w http.ResponseWriter, r *http.Request) {
 	wiiID := r.Form.Get("mlid")
 	if !friendCodeIsValid(wiiID) {
-		w.Header().Add("cd", "610")
 		fmt.Fprint(w, GenNormalErrorCode(610, "Invalid Wii Friend Code."))
 		return
 	}
@@ -21,7 +20,6 @@ func Account(w http.ResponseWriter, r *http.Request) {
 
 	stmt, err := db.Prepare("INSERT IGNORE INTO `accounts` (`mlid`,`passwd`, `mlchkid` ) VALUES (?, ?, ?)")
 	if err != nil {
-		w.Header().Add("cd", "410")
 		fmt.Fprint(w, GenNormalErrorCode(410, "Database error."))
 		log.Fatal(err)
 		return
@@ -37,7 +35,6 @@ func Account(w http.ResponseWriter, r *http.Request) {
 
 	result, err := stmt.Exec(wiiID, passwdHash, mlchkidHash)
 	if err != nil {
-		w.Header().Add("cd", "410")
 		fmt.Fprint(w, GenNormalErrorCode(410, "Database error."))
 		log.Println(err)
 		return
@@ -45,22 +42,17 @@ func Account(w http.ResponseWriter, r *http.Request) {
 
 	affected, err := result.RowsAffected()
 	if err != nil {
-		w.Header().Add("cd", "410")
 		fmt.Fprint(w, GenNormalErrorCode(410, "Database error."))
 		log.Println(err)
 		return
 	}
 
 	if affected == 0 {
-		w.Header().Add("cd", "211")
 		fmt.Fprint(w, "\n",
 			GenNormalErrorCode(211, "Duplicate registration."))
 		return
 	}
 
-	w.Header().Add("cd", "100")
-	w.Header().Add("mlchkid", mlchkid)
-	w.Header().Add("passwd", passwd)
 	fmt.Fprint(w, "\n",
 		GenNormalErrorCode(100, "Success."),
 		"mlid=", wiiID, "\n",
